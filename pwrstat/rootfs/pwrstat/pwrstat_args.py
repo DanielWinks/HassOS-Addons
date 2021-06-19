07326ea0519184736f0401a594cb782746ddbefc
Daniel Winks <daniel.winks@gmail.com>
2021-06-18 22:52:08 -0400

    Update pwrstat_args.py

--- pwrstat/rootfs/pwrstat/pwrstat_args.py
+++ pwrstat/rootfs/pwrstat/pwrstat_args.py
@@ -1,25 +1,55 @@
 import argparse
+from typing import Any, Dict
 
-parser = argparse.ArgumentParser()
+_parser = argparse.ArgumentParser()
 
-parser.add_argument("-mb", "--mqtt_broker", help="MQTT Broker IP Address/DNS Name")
-parser.add_argument("-mp", "--mqtt_port", help="MQTT Port")
-parser.add_argument("-mc", "--mqtt_clientid", help="MQTT Client ID")
-parser.add_argument("-mt", "--mqtt_topic", help="MQTT Topic")
-parser.add_argument("-mf", "--mqtt_refresh", help="MQTT Refresh Interval (seconds)")
-parser.add_argument("-mq", "--mqtt_qos", help="MQTT QOS")
-parser.add_argument("-mr", "--mqtt_retained", help="MQTT Retained Flag")
-parser.add_argument("-mu", "--mqtt_username", help="MQTT Username")
-parser.add_argument("-mw", "--mqtt_password", help="MQTT Password")
+_parser.add_argument("-pl", "--pwrstat_api_log_level", help="Pwrstat API Logging Level")
 
-parser.add_argument("-rp", "--rest_port", help="REST Endpoint Port")
-parser.add_argument("-rb", "--rest_bind_address", help="REST Bind Address")
+_parser.add_argument("-mb", "--mqtt_broker", help="MQTT Broker IP Address/DNS Name")
+_parser.add_argument("-mp", "--mqtt_port", help="MQTT Port")
+_parser.add_argument("-mc", "--mqtt_clientid", help="MQTT Client ID")
+_parser.add_argument("-mt", "--mqtt_topic", help="MQTT Topic")
+_parser.add_argument("-mf", "--mqtt_refresh", help="MQTT Refresh Interval (seconds)")
+_parser.add_argument("-mq", "--mqtt_qos", help="MQTT QOS")
+_parser.add_argument("-mr", "--mqtt_retained", help="MQTT Retained Flag")
+_parser.add_argument("-mu", "--mqtt_username", help="MQTT Username")
+_parser.add_argument("-mw", "--mqtt_password", help="MQTT Password")
 
-parser.add_argument("-pp", "--prom_port", help="Prometheus Port")
-parser.add_argument("-pb", "--prom_bind_address", help="Prometheus Bind Address")
-parser.add_argument("-pl", "--prom_labels", help="Prometheus Labels")
+_parser.add_argument("-rp", "--rest_port", help="REST Endpoint Port")
+_parser.add_argument("-rb", "--rest_bind_address", help="REST Bind Address")
 
-args = parser.parse_args()
+_parser.add_argument("-pp", "--prom_port", help="Prometheus Port")
+_parser.add_argument("-pb", "--prom_bind_address", help="Prometheus Bind Address")
+_parser.add_argument("-pl", "--prom_labels", help="Prometheus Labels")
 
-# if __name__ == "__main__":
-#     pass
+_args = vars(_parser.parse__args())
+
+_pwrstat_api_config: Dict[str, Any] = {}
+_mqtt_config: Dict[str, Any] = {}
+_rest_config: Dict[str, str] = {}
+_prometheus_config: Dict[str, Any] = {}
+
+_pwrstat_api_config["log_level"] = _args["pwrstat_api_log_level"]
+
+_mqtt_config["broker"] = _args["mqtt_broker"]
+_mqtt_config["port"] = _args["mqtt_port"]
+_mqtt_config["client_id"] = _args["mqtt_clientid"]
+_mqtt_config["topic"] = _args["mqtt_topic"]
+_mqtt_config["refresh"] = _args["mqtt_refresh"]
+_mqtt_config["qos"] = _args["mqtt_qos"]
+_mqtt_config["retained"] = _args["mqtt_retained"]
+_mqtt_config["username"] = _args["mqtt_username"]
+_mqtt_config["password"] = _args["mqtt_password"]
+
+_rest_config["port"] = _args["rest_port"]
+_rest_config["bind_address"] = _args["rest_bind_address"]
+
+_prometheus_config["port"] = _args["prom_port"]
+_prometheus_config["bind_address"] = _args["prom_bind_address"]
+_prometheus_config["labels"] = _args["prom_labels"]
+
+config: Dict[str, Any]= {}
+config["pwrstat_api"] = _pwrstat_api_config
+config["mqtt"] = _mqtt_config
+config["rest"] = _rest_config
+config["prometheus"] = _prometheus_config
