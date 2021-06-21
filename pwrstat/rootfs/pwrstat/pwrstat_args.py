cc289f028c96ce8feb493589c48df24e09c83588
Daniel Winks <daniel.winks@gmail.com>
2021-06-20 23:35:36 -0400

    Update pwrstat_args.py

--- pwrstat/rootfs/pwrstat/pwrstat_args.py
+++ pwrstat/rootfs/pwrstat/pwrstat_args.py
@@ -22,7 +22,7 @@ _parser.add_argument("-pp", "--prom_port", help="Prometheus Port")
 _parser.add_argument("-pb", "--prom_bind_address", help="Prometheus Bind Address")
 _parser.add_argument("-pl", "--prom_labels", help="Prometheus Labels")
 
-_args = vars(_parser.parse__args())
+_args = vars(_parser.parse_args())
 
 _pwrstat_api_config: Dict[str, Any] = {}
 _mqtt_config: Dict[str, Any] = {}
@@ -32,19 +32,19 @@ _prometheus_config: Dict[str, Any] = {}
 _pwrstat_api_config["log_level"] = _args["pwrstat_api_log_level"]
 
 _mqtt_config["broker"] = _args["mqtt_broker"]
-_mqtt_config["port"] = _args["mqtt_port"]
+_mqtt_config["port"] = int(_args["mqtt_port"])
 _mqtt_config["client_id"] = _args["mqtt_clientid"]
 _mqtt_config["topic"] = _args["mqtt_topic"]
-_mqtt_config["refresh"] = _args["mqtt_refresh"]
-_mqtt_config["qos"] = _args["mqtt_qos"]
+_mqtt_config["refresh"] = int(_args["mqtt_refresh"])
+_mqtt_config["qos"] = int(_args["mqtt_qos"])
 _mqtt_config["retained"] = _args["mqtt_retained"]
 _mqtt_config["username"] = _args["mqtt_username"]
 _mqtt_config["password"] = _args["mqtt_password"]
 
-_rest_config["port"] = _args["rest_port"]
+_rest_config["port"] = int(_args["rest_port"])
 _rest_config["bind_address"] = _args["rest_bind_address"]
 
-_prometheus_config["port"] = _args["prom_port"]
+_prometheus_config["port"] = int(_args["prom_port"])
 _prometheus_config["bind_address"] = _args["prom_bind_address"]
 _prometheus_config["labels"] = _args["prom_labels"]
 
