37c1d7c0222b07027d4e88d6e4ca97534cd3cea0
Daniel Winks <daniel.winks@gmail.com>
2021-07-23 17:51:57 -0400

    Update run.sh

--- snapserver/rootfs/run.sh
+++ snapserver/rootfs/run.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/with-contenv bashio
+declare username
+declare password
+
+if bashio::config.has_value "spotify_user"; then
+  username=$(bashio::config 'spotify_user')
+  if bashio::config.has_value "spotify_pw"; then
+    password=$(bashio::config 'spotify_pw')
+
+    echo "stream = librespot:///librespot?name=Spotify &" >>/etc/snapserver.conf
+    echo "dryout_ms=2000 &" >>/etc/snapserver.conf
+    echo "username=${username} &" >>/etc/snapserver.conf
+    echo "password=${password} &" >>/etc/snapserver.conf
+    echo "devicename=Snapcast &" >>/etc/snapserver.conf
+    echo "bitrate=320 &" >>/etc/snapserver.conf
+    echo "wd_timeout=7800 &" >>/etc/snapserver.conf
+    echo "volume=100 &" >>/etc/snapserver.conf
+    echo "nomalize=true &" >>/etc/snapserver.conf
+    echo "autoplay=false" >>/etc/snapserver.conf
+    echo "stream = meta:///mopidy_tcp/Spotify?name=Meta" >>/etc/snapserver.conf
+  fi
+fi
+
 snapserver
