00fc86952fae22b9f7fbea9cf2eade31cc408129
Daniel Winks <daniel.winks@gmail.com>
2021-07-26 13:56:38 -0400

    .

--- owntone/rootfs/etc/cont-init.d/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-# bashio::var.json \
-#   interface "$(bashio::addon.ip_address)" \
-#   entry "$(bashio::addon.ingress_entry)" |
-#   tempio \
-#     -template /etc/nginx/templates/ingress.gtpl \
-#     -out /etc/nginx/servers/ingress.conf
