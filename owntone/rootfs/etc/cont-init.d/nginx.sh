6d7ecd991b4d8fb72855fed8f214c74bc1f868c3
Daniel Winks <daniel.winks@gmail.com>
2021-07-26 01:18:40 -0400

    fixes

--- owntone/rootfs/etc/cont-init.d/nginx.sh
+++ owntone/rootfs/etc/cont-init.d/nginx.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Ulrar Hass.io Add-ons: forked-daapd server
-# Configures the forked-daapd Server
-# ==============================================================================
 
-declare ingress_interface
-declare ingress_port
-declare ingress_entry
-
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
+bashio::var.json \
+  interface "$(bashio::addon.ip_address)" \
+  entry "$(bashio::addon.ingress_entry)" |
+  tempio \
+    -template /etc/nginx/templates/ingress.gtpl \
+    -out /etc/nginx/servers/ingress.conf
