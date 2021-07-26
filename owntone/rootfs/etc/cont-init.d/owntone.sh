5a57bd725b8a1c85c67e54fe316d18f9e4fcba1d
Daniel Winks <daniel.winks@gmail.com>
2021-07-25 23:54:47 -0400

    Update owntone.sh

--- owntone/rootfs/etc/cont-init.d/owntone.sh
+++ owntone/rootfs/etc/cont-init.d/owntone.sh
@@ -7,7 +7,7 @@ fi
 
 if ! bashio::fs.file_exists '/config/owntone/owntone.conf'; then
     bashio::log.debug 'Copying default conf file...'
-    cp /usr/local/etc/owntone.conf /config/owntone/owntone.conf
+    cp /etc/owntone.conf.orig /config/owntone/owntone.conf
 fi
 
 if ! bashio::fs.directory_exists '/config/owntone/music'; then
