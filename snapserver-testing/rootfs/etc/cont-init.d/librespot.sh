9fa0db1492d16d5b1064351f77033783b3cff30b
Daniel Winks <daniel.winks@gmail.com>
2021-08-03 14:57:56 -0400

    .

--- snapserver-testing/rootfs/etc/cont-init.d/librespot.sh
+++ snapserver-testing/rootfs/etc/cont-init.d/librespot.sh
@@ -4,6 +4,7 @@ declare pipe
 declare device_type
 declare bitrate
 declare additional_opts
+declare json_values
 
 if bashio::config.true "start_librespot"; then
   for librespot in $(bashio::config 'librespot_instances|keys'); do
@@ -28,21 +29,29 @@ if bashio::config.true "start_librespot"; then
 
     if bashio::config.has_value 'additional_opts'; then
       additional_opts=$(bashio::config 'additional_opts')
-    else
-      additional_opts=" "
     fi
     bashio::log.info "Addional Options: ${additional_opts}"
 
     path=$(echo ${name} | sed -f /etc/url_escape.sed)
     mkdir /etc/services.d/${path}
 
-    json=$(bashio::var.json \
-      name ${name} \
-      pipe ${pipe} \
-      device_type ${device_type} \
-      bitrate ${bitrate} \
-      additional_opts ${additional_opts})
-    echo ${json} |
+    if bashio::config.has_value 'additional_opts'; then
+      json_values=$(bashio::var.json \
+        name ${name} \
+        pipe ${pipe} \
+        device_type ${device_type} \
+        bitrate ${bitrate} \
+        additional_opts ${additional_opts})
+    else
+      json_values=$(bashio::var.json \
+        name ${name} \
+        pipe ${pipe} \
+        device_type ${device_type} \
+        bitrate ${bitrate} \
+        additional_opts "")
+    fi
+
+    echo ${json_values} |
       tempio \
         -template /etc/librespot/template.gtpl \
         -out /etc/services.d/${path}/run
