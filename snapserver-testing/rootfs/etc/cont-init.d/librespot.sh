284d7a19861eddc282c13de78febd3ab91796807
Daniel Winks <daniel.winks@gmail.com>
2021-08-03 12:35:14 -0400

    .

--- snapserver-testing/rootfs/etc/cont-init.d/librespot.sh
+++ snapserver-testing/rootfs/etc/cont-init.d/librespot.sh
@@ -1,40 +1,37 @@
 #!/usr/bin/with-contenv bashio
 declare name
 declare pipe
-declare silence
 declare device_type
 declare bitrate
 declare additional_opts
-
-silence=""
-if bashio::config.true "silence_librespot"; then
-  silence=">/dev/null 2>&1"
-  bashio::log.info "Silencing Librespot"
-fi
 
 if bashio::config.true "start_librespot"; then
   for librespot in $(bashio::config 'librespot_instances|keys'); do
 
     name=$(bashio::config "librespot_instances[${librespot}].name")
     pipe=$(bashio::config "librespot_instances[${librespot}].pipe")
+    bashio::log.info "Processing ${name} at ${pipe}"
 
     if bashio::config.has_value 'device_type'; then
       device_type=$(bashio::config 'device_type')
     else
       device_type="speaker"
     fi
+    bashio::log.info "Device Type: ${device_type}"
 
     if bashio::config.has_value 'bitrate'; then
       bitrate=$(bashio::config 'bitrate')
     else
       bitrate="320"
     fi
+    bashio::log.info "Bitrate: ${bitrate}"
 
     if bashio::config.has_value 'additional_opts'; then
       additional_opts=$(bashio::config 'additional_opts')
     else
       additional_opts=""
     fi
+    bashio::log.info "Addional Options: ${additional_opts}"
 
     path=$(echo ${name} | sed -f /etc/url_escape.sed)
     mkdir /etc/services.d/${path}
